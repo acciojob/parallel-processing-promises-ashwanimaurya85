01084fca4e94c686401b5bfa6c46c52ab84c1777
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-08-28 15:30:51 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,30 @@
+const output = document.getElementById("output");
+const btn = document.getElementById("download-images-button");
+
+const images = [
+  { url: "https://picsum.photos/id/237/200/300" },
+  { url: "https://picsum.photos/id/238/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" },
+];
+
+btn.addEventListener("click", () => {
+  const imagePromises = images.map(image => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+      img.src = image.url;
+    });
+  });
+
+  Promise.all(imagePromises)
+    .then(images => {
+      output.innerHTML = "";
+      images.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
